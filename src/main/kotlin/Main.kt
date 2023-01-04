2b09a747ae6eb5aa88f1939ed418b56200049fae
Patrick Mendonca <mendonca.patrick@yahoo.com>
2023-01-04 21:05:40 +0800

    more tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,18 @@
 fun main() {
-    var i = 0
-    while (i <= 50){
-        println(i)
-        i++
-    }
-
-
-    fuckass@ do {
-        var j = 15
-        while (j > 10) {
-            println("---${--j}")
-            if (j == 11) break@fuckass
-        }
-        i--
-        println(i)
-    } while (i != 3)
-
-    byTwo(38385)
-
+    greet("Herbie", 4)
+    println(getMax(14, 1))
+    println(double(37567))
 }
 
-fun byTwo(myInput: Int) {
-    var input = myInput
-    print("$input is divisible by 2")
-    var k = 0
-    while (input >= 2) {
-        k++
-        input /= 2
-        when {
-            input > 1000 -> println("Still pretty big...")
-            input in 100..999 -> println("Getting smaller now...")
-            input < 100 -> println("Almost done!")
-        }
+fun double(i: Int) = i * 2
+
+fun getMax(i: Int, j: Int) = if (i >= j) i else j
+
+/***
+ * greets the passed name, repeats the greeting reps times, default 1
+ */
+fun greet(name: String, reps: Int = 0) {
+    for (i in 0 until reps) {
+        println("Hello $name, nice to meet you!")
     }
-    println(" $k times.")
 }
