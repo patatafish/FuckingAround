aeb984d77362da18cc41a683fcff050471082a08
Patrick Mendonca <mendonca.patrick@yahoo.com>
2023-01-04 22:20:55 +0800

    more tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
 fun main() {
+    val test = Test()
+    println(test.name)
+    println(test.age)
 
+    test.name = "                   Farts"
+    println(test.name)
+
+    val biotch = Test("     mike hawk             ", 69)
+    println(biotch.name)
+    println(biotch.age)
+
+    val fucks = Test(age = 100)
+    println(fucks.age)
+
+    val five = Test("            ", 34)
+    println("${five.name} ${five.age}")
+}
+
+class Test(name: String = "Default User", var age: Int = -1) {
+    var name: String = if (name.isBlank()) "Default User" else name.trim()
+
+    init {
+        println("I've shat my pants")
+
+    }
 }
