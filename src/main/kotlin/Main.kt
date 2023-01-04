3c15d2be6048344eddc5611e2b02248698d46fd7
Patrick Mendonca <mendonca.patrick@yahoo.com>
2023-01-04 20:29:21 +0800

    more tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,28 @@ fun main() {
         var j = 15
         while (j > 10) {
             println("---${--j}")
-            if (j == 11) continue@fuckass
+            if (j == 11) break@fuckass
         }
         i--
         println(i)
     } while (i != 3)
 
+    byTwo(38385)
+
+}
+
+fun byTwo(myInput: Int) {
+    var input = myInput
+    print("$input is divisible by 2")
+    var k = 0
+    while (input >= 2) {
+        k++
+        input /= 2
+        when {
+            input > 1000 -> println("Still pretty big...")
+            input in 100..999 -> println("Getting smaller now...")
+            input < 100 -> println("Almost done!")
+        }
+    }
+    println(" $k times.")
 }
